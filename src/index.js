da5eb7aba1ed0ed5e7f89ad8a32c3d8daff2ae41
+ <+>
2023-01-31 09:48:30 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+function Welcome(props){
+    return <h1>Hello, {props.name}</h1>;
+}
